981165568384c252bd337b51fc66cdd3e678c99e
koudounasalkis <alkis.koudounas@polito.it>
2025-05-21 17:27:43 +0000

    compute scores bash

--- src/compute_shallow_scores.sh
+++ src/compute_shallow_scores.sh
@@ -5,8 +5,8 @@ MODEL_NAME=canary1b
 CUDA_VISIBLE_DEVICES=$CUDA_DEVICE python main.py \
     --dataset_name $DATASET_NAME \
     --model_name $MODEL_NAME \
-    --gt_transcriptions_path /home/akoudounas/SHALLOW-old/gt/${DATASET_NAME}_gt.txt \
-    --predictions_path /home/akoudounas/SHALLOW-old/inference/${DATASET_NAME}/${DATASET_NAME}_${MODEL_NAME}_zs.txt \
-    --output_dir /home/akoudounas/SHALLOW/results/ \
+    --gt_transcriptions_path ./gt/${DATASET_NAME}_gt.txt \
+    --predictions_path ./inference/${DATASET_NAME}/${DATASET_NAME}_${MODEL_NAME}_zs.txt \
+    --output_dir ./results/ \
     --examples_limit 50 \
     --num_workers 2
