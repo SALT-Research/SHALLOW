d563485ff5ff600aa7949e6ac1cdca17ad49b546
koudounasalkis <alkis.koudounas@polito.it>
2025-05-21 17:40:29 +0000

    shallow

--- src/compute_shallow_scores.sh
+++ src/compute_shallow_scores.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=$CUDA_DEVICE python main.py \
     --dataset_name $DATASET_NAME \
     --model_name $MODEL_NAME \
     --gt_transcriptions_path ./gt/${DATASET_NAME}_gt.txt \
-    --predictions_path ./inference/${DATASET_NAME}/${DATASET_NAME}_${MODEL_NAME}_zs.txt \
+    --predictions_path ./inference/${DATASET_NAME}/${DATASET_NAME}_${MODEL_NAME}.txt \
     --output_dir ./results/ \
-    --examples_limit 50 \
-    --num_workers 2
+    --examples_limit -1 \
+    --num_workers 4
